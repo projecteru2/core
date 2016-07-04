453bb9d07fe97a9a7db50c7b6cedd11884434eb9
tonic <tonicbupt@gmail.com>
2016-07-04 14:45:35 +0800

    add lock in case that read/write concurrency on the same map

--- utils/utils.go
+++ utils/utils.go
@@ -3,17 +3,10 @@ package utils
 import (
 	"fmt"
 	"math/rand"
-	"net"
-	"net/http"
-	"net/url"
-	"path/filepath"
 	"strings"
 	"time"
 
-	log "github.com/Sirupsen/logrus"
 	engineapi "github.com/docker/engine-api/client"
-	"github.com/docker/go-connections/tlsconfig"
-	"gitlab.ricebook.net/platform/core/types"
 	"golang.org/x/net/context"
 )
 
@@ -41,71 +34,6 @@ func TruncateID(id string) string {
 func Tail(path string) string {
 	parts := strings.Split(path, "/")
 	return parts[len(parts)-1]
-}
-
-// cache connections
-// otherwise they'll leak
-var clients = make(map[string]*engineapi.Client)
-
-func MakeDockerClient(endpoint string, config types.Config, force bool) (*engineapi.Client, error) {
-	if !strings.HasPrefix(endpoint, "tcp://") {
-		endpoint = "tcp://" + endpoint
-	}
-
-	u, err := url.Parse(endpoint)
-	if err != nil {
-		return nil, err
-	}
-
-	host, _, err := net.SplitHostPort(u.Host)
-	if err != nil {
-		return nil, err
-	}
-
-	// try get client, if nil, create a new one
-	client := clients[host]
-	if client == nil || force {
-		// if no cert path is set
-		// then just use normal http client without tls
-		var cli *http.Client
-		if config.Docker.CertPath != "" {
-			dockerCertPath := filepath.Join(config.Docker.CertPath, host)
-			options := tlsconfig.Options{
-				CAFile:             filepath.Join(dockerCertPath, "ca.pem"),
-				CertFile:           filepath.Join(dockerCertPath, "cert.pem"),
-				KeyFile:            filepath.Join(dockerCertPath, "key.pem"),
-				InsecureSkipVerify: false,
-			}
-			tlsc, err := tlsconfig.Client(options)
-			if err != nil {
-				return nil, err
-			}
-
-			cli = &http.Client{
-				Transport: &http.Transport{
-					TLSClientConfig: tlsc,
-				},
-			}
-		}
-
-		log.Debugf("Create new http.Client for %q", endpoint)
-		client, err = engineapi.NewClient(endpoint, config.Docker.APIVersion, cli, nil)
-		if err != nil {
-			return nil, err
-		}
-
-		clients[host] = client
-	}
-
-	// timeout in 5 seconds
-	// timeout means node is not available
-	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
-	_, err = client.Info(ctx)
-	if err != nil {
-		return nil, err
-	}
-
-	return client, nil
 }
 
 func GetGitRepoName(url string) (string, error) {
