6f8ab673dd7492042aa680a774076b6eac52c46a
Zhang Ye <zhangye@ricebook.com>
2016-07-18 15:40:39 +0800

    update
    1. 更均匀的averagePlan

--- scheduler/complex/potassium_test.go
+++ scheduler/complex/potassium_test.go
@@ -61,7 +61,8 @@ func TestSelectNodes(t *testing.T) {
 
 	r, re, err := k.SelectNodes(nodes, 1, 2)
 	assert.NoError(t, err)
-	assert.Equal(t, 1, len(re))
+	assert.Equal(t, 2, len(r))
+	assert.Equal(t, 2, len(re))
 
 	for nodename, cpus := range r {
 		assert.Contains(t, []string{"node1", "node2"}, nodename)
@@ -84,7 +85,7 @@ func TestSelectNodes(t *testing.T) {
 		},
 	}
 
-	r, re, err = k.SelectNodes(nodes, 1.3, 1)
+	r, re, err = k.SelectNodes(nodes, 1.3, 2)
 	assert.NoError(t, err)
 
 	for nodename, cpus := range r {
@@ -94,7 +95,6 @@ func TestSelectNodes(t *testing.T) {
 		cpu := cpus[0]
 		assert.Equal(t, cpu.Total(), 13)
 	}
-
 }
 
 func checkAvgPlan(res map[string][]types.CPUMap, minCon int, maxCon int, name string) error {
@@ -112,6 +112,8 @@ func checkAvgPlan(res map[string][]types.CPUMap, minCon int, maxCon int, name st
 	}
 	if minC != minCon || maxC != maxCon {
 		fmt.Println(name)
+		fmt.Println("min: ", minC)
+		fmt.Println("max: ", maxC)
 		for k, v := range res {
 			fmt.Println(k, ":", len(v))
 		}
@@ -166,7 +168,6 @@ func TestComplexNodes(t *testing.T) {
 	}
 
 	k, _ = New(coreCfg)
-
 	// test1
 	res1, changed1, err := k.SelectNodes(nodes, 1.7, 7)
 	if err != nil {
@@ -175,7 +176,7 @@ func TestComplexNodes(t *testing.T) {
 	if check := checkAvgPlan(res1, 1, 2, "res1"); check != nil {
 		t.Fatalf("something went wrong")
 	}
-	assert.Equal(t, len(changed1), 4)
+	assert.Equal(t, len(changed1), len(res1))
 
 	// test2
 	// SelectNodes 里有一些副作用, 粗暴地拿一个新的来测试吧
@@ -207,11 +208,14 @@ func TestComplexNodes(t *testing.T) {
 			"4": 10, "5": 10, "6": 10, "7": 10,
 		},
 	}
-	res2, rem2, err := k.SelectNodes(nodes, 1.7, 11)
-	if check := checkAvgPlan(res2, 1, 4, "res2"); check != nil {
+	res2, changed2, err := k.SelectNodes(nodes, 1.7, 11)
+	if err != nil {
 		t.Fatalf("something went wrong")
 	}
-	assert.Equal(t, len(rem2), 4)
+	if check := checkAvgPlan(res2, 2, 3, "res2"); check != nil {
+		t.Fatalf("something went wrong")
+	}
+	assert.Equal(t, len(changed2), len(res2))
 
 	// test3
 	nodes = map[string]types.CPUMap{
@@ -241,11 +245,15 @@ func TestComplexNodes(t *testing.T) {
 			"4": 10, "5": 10, "6": 10, "7": 10,
 		},
 	}
-	res3, rem3, err := k.SelectNodes(nodes, 1.7, 23)
+	res3, changed3, err := k.SelectNodes(nodes, 1.7, 23)
+	if err != nil {
+		fmt.Println("May be we dont have plan")
+		fmt.Println(err)
+	}
 	if check := checkAvgPlan(res3, 2, 6, "res3"); check != nil {
 		t.Fatalf("something went wrong")
 	}
-	assert.Equal(t, len(rem3), 5)
+	assert.Equal(t, len(changed3), len(res3))
 
 	// test4
 	nodes = map[string]types.CPUMap{
@@ -279,4 +287,172 @@ func TestComplexNodes(t *testing.T) {
 	if newErr == nil {
 		t.Fatalf("how to alloc 29 containers when you only have 28?")
 	}
+}
+
+func TestEvenPlan(t *testing.T) {
+	coreCfg := types.Config{
+		EtcdMachines:   []string{"http://127.0.0.1:2379"},
+		EtcdLockPrefix: "/eru-core/_lock",
+		Scheduler: types.SchedConfig{
+			LockKey: "/coretest",
+			LockTTL: 1,
+			Type:    "complex",
+		},
+	}
+
+	k, merr := New(coreCfg)
+	if merr != nil {
+		t.Fatalf("cannot create Potassim instance.", merr)
+	}
+
+	// nodes -- n1: 2, n2: 2
+	pod1 := map[string]types.CPUMap{
+		"n1": types.CPUMap{ // 2 containers
+			"0": 10, "1": 10, "2": 10, "3": 10,
+		},
+		"n2": types.CPUMap{ // 2 containers
+			"0": 10, "1": 10, "2": 10, "3": 10,
+		},
+	}
+
+	res1, rem1, err := k.SelectNodes(pod1, 1.3, 2)
+	if err != nil {
+		t.Fatalf("sth wrong")
+	}
+	if check := checkAvgPlan(res1, 1, 1, "res1"); check != nil {
+		t.Fatalf("something went wrong")
+	}
+	assert.Equal(t, len(rem1), 2)
+
+	// nodes -- n1: 4, n2: 5, n3:6, n4: 5
+	pod2 := map[string]types.CPUMap{
+		"n1": types.CPUMap{ // 4 containers
+			"0": 10, "1": 10, "2": 10, "3": 10,
+			"4": 10, "5": 10, "6": 10, "7": 10,
+		},
+		"n2": types.CPUMap{ // 5 containers
+			"0": 10, "1": 10, "2": 10, "3": 10,
+			"4": 10, "5": 10, "6": 10, "7": 10,
+			"8": 10, "9": 10,
+		},
+		"n3": types.CPUMap{ // 6 containers
+			"0": 10, "1": 10, "2": 10, "3": 10,
+			"4": 10, "5": 10, "6": 10, "7": 10,
+			"8": 10, "9": 10, "10": 10, "11": 10,
+		},
+		"n4": types.CPUMap{ // 5 containers
+			"0": 10, "1": 10, "2": 10, "3": 10,
+			"4": 10, "5": 10, "6": 10, "7": 10,
+			"8": 10, "9": 10,
+		},
+	}
+
+	res2, rem2, err := k.SelectNodes(pod2, 1.7, 3)
+	if check := checkAvgPlan(res2, 1, 1, "res2"); check != nil {
+		t.Fatalf("something went wront")
+	}
+	assert.Equal(t, len(rem2), 3)
+
+	pod3 := map[string]types.CPUMap{
+		"n1": types.CPUMap{ // 4 containers
+			"0": 10, "1": 10, "2": 10, "3": 10,
+			"4": 10, "5": 10, "6": 10, "7": 10,
+		},
+		"n2": types.CPUMap{ // 5 containers
+			"0": 10, "1": 10, "2": 10, "3": 10,
+			"4": 10, "5": 10, "6": 10, "7": 10,
+			"8": 10, "9": 10,
+		},
+		"n3": types.CPUMap{ // 6 containers
+			"0": 10, "1": 10, "2": 10, "3": 10,
+			"4": 10, "5": 10, "6": 10, "7": 10,
+			"8": 10, "9": 10, "10": 10, "11": 10,
+		},
+		"n4": types.CPUMap{ // 5 containers
+			"0": 10, "1": 10, "2": 10, "3": 10,
+			"4": 10, "5": 10, "6": 10, "7": 10,
+			"8": 10, "9": 10,
+		},
+	}
+	res3, rem3, err := k.SelectNodes(pod3, 1.7, 8)
+	if check := checkAvgPlan(res3, 2, 2, "res3"); check != nil {
+		t.Fatalf("something went wront")
+	}
+	assert.Equal(t, len(rem3), 4)
+
+	pod4 := map[string]types.CPUMap{
+		"n1": types.CPUMap{ // 4 containers
+			"0": 10, "1": 10, "2": 10, "3": 10,
+			"4": 10, "5": 10, "6": 10, "7": 10,
+		},
+		"n2": types.CPUMap{ // 5 containers
+			"0": 10, "1": 10, "2": 10, "3": 10,
+			"4": 10, "5": 10, "6": 10, "7": 10,
+			"8": 10, "9": 10,
+		},
+		"n3": types.CPUMap{ // 6 containers
+			"0": 10, "1": 10, "2": 10, "3": 10,
+			"4": 10, "5": 10, "6": 10, "7": 10,
+			"8": 10, "9": 10, "10": 10, "11": 10,
+		},
+		"n4": types.CPUMap{ // 5 containers
+			"0": 10, "1": 10, "2": 10, "3": 10,
+			"4": 10, "5": 10, "6": 10, "7": 10,
+			"8": 10, "9": 10,
+		},
+	}
+
+	res4, rem4, err := k.SelectNodes(pod4, 1.7, 10)
+	if check := checkAvgPlan(res4, 2, 3, "res4"); check != nil {
+		t.Fatalf("something went wrong")
+	}
+	assert.Equal(t, len(rem4), 4)
+}
+
+func TestSpecialCase(t *testing.T) {
+	pod := map[string]types.CPUMap{
+		"n1": types.CPUMap{ // 1 containers
+			"0": 10, "1": 10,
+		},
+		"n2": types.CPUMap{ // 3 containers
+			"0": 10, "1": 10, "2": 10, "3": 10,
+			"4": 10, "5": 10,
+		},
+		"n3": types.CPUMap{ // 4 containers
+			"0": 10, "1": 10, "2": 10, "3": 10,
+			"4": 10, "5": 10, "6": 10, "7": 10,
+		},
+	}
+
+	coreCfg := types.Config{
+		EtcdMachines:   []string{"http://127.0.0.1:2379"},
+		EtcdLockPrefix: "/eru-core/_lock",
+		Scheduler: types.SchedConfig{
+			LockKey: "/coretest",
+			LockTTL: 1,
+			Type:    "complex",
+		},
+	}
+
+	k, _ := New(coreCfg)
+	res1, _, err := k.SelectNodes(pod, 1.7, 7)
+	if err != nil {
+		t.Fatalf("something went wrong")
+	}
+	checkAvgPlan(res1, 1, 3, "new test 2")
+
+	newpod := map[string]types.CPUMap{
+		"n1": types.CPUMap{ // 3 containers
+			"0": 10, "1": 10, "2": 10, "3": 10,
+			"4": 10, "5": 10,
+		},
+		"n2": types.CPUMap{ // 4 containers
+			"0": 10, "1": 10, "2": 10, "3": 10,
+			"4": 10, "5": 10, "6": 10, "7": 10,
+		},
+	}
+
+	res2, changed2, _ := k.SelectNodes(newpod, 1.7, 4)
+	assert.Equal(t, len(res2), len(changed2))
+	checkAvgPlan(res2, 2, 2, "new test 2")
 }
