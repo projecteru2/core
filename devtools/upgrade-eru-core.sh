f0a4cbc3f0377c1d411b762130f9569c6769a84a
timfeirg <kkcocogogo@gmail.com>
2016-12-02 19:27:39 +0800

    version bump

--- devtools/upgrade-eru-core.sh
+++ devtools/upgrade-eru-core.sh
@@ -6,12 +6,14 @@ deploy_mode=${1-test}
 if [ $deploy_mode == "test" ]
 then
   ssh c1-eru-1.ricebook.link -t 'sudo yum --enablerepo=ricebook clean metadata'
+  ssh c1-eru-1.ricebook.link -t 'sudo yum makecache'
   ssh c1-eru-1.ricebook.link -t 'sudo yum reinstall -y eru-core'
   ssh c1-eru-1.ricebook.link -t 'sudo systemctl daemon-reload'
   ssh c1-eru-1.ricebook.link -t 'sudo systemctl restart eru-core.service'
 elif [ $deploy_mode == "prod" ]
 then
   ssh c2-eru-1.ricebook.link -t 'sudo yum --enablerepo=ricebook clean metadata'
+  ssh c1-eru-1.ricebook.link -t 'sudo yum makecache'
   ssh c2-eru-1.ricebook.link -t 'sudo yum reinstall -y eru-core'
   ssh c2-eru-1.ricebook.link -t 'sudo systemctl daemon-reload'
   ssh c2-eru-1.ricebook.link -t 'sudo systemctl restart eru-core.service'
