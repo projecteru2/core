74a1a3adce40b5ea22e50498e5323e9af2cc9ed9
timfeirg <kkcocogogo@gmail.com>
2017-02-03 12:23:28 +0800

    deploy script bug

--- devtools/upgrade-eru-core.sh
+++ devtools/upgrade-eru-core.sh
@@ -23,6 +23,14 @@ then
   sudo systemctl daemon-reload
   sudo systemctl restart eru-core.service
 EOF
+  ssh c1-eru-1 << EOF
+  sudo yum --enablerepo=ricebook clean metadata
+  sudo yum makecache
+  sudo yum remove -y eru-core
+  sudo yum install -y eru-core
+  sudo systemctl daemon-reload
+  sudo systemctl restart eru-core.service
+EOF
 else
   exit 127
 fi
