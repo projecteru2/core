8fb20e99fcc5290dd8f3aa54bef2d8f14b83b185
CMGS <ilskdw@gmail.com>
2017-10-09 19:29:40 +0800

    clean useless make container config

--- cluster/calcium/helper.go
+++ cluster/calcium/helper.go
@@ -9,7 +9,6 @@ import (
 
 	enginetypes "github.com/docker/docker/api/types"
 	enginecontainer "github.com/docker/docker/api/types/container"
-	enginenetwork "github.com/docker/docker/api/types/network"
 	engineapi "github.com/docker/docker/client"
 	"github.com/projecteru2/core/lock"
 	"github.com/projecteru2/core/types"
@@ -102,47 +101,6 @@ func ensureReaderClosed(stream io.ReadCloser) {
 	}
 	io.Copy(ioutil.Discard, stream)
 	stream.Close()
-}
-
-// Copies config from container
-// And make a new name for container
-func makeContainerConfig(info enginetypes.ContainerJSON, image string) (
-	*enginecontainer.Config,
-	*enginecontainer.HostConfig,
-	*enginenetwork.NetworkingConfig,
-	string,
-	error) {
-
-	// we use `_` to join container name
-	// since we don't support `_` in entrypoint, and no `_` is in suffix,
-	// the last part will be suffix and second last part will be entrypoint,
-	// the rest will be the appname
-	parts := strings.Split(trimLeftSlash(info.Name), "_")
-	length := len(parts)
-	if length < 3 {
-		return nil, nil, nil, "", fmt.Errorf("Bad container name format: %q", info.Name)
-	}
-
-	entrypoint := parts[length-2]
-	appname := strings.Join(parts[:length-2], "_")
-
-	suffix := utils.RandomString(6)
-	containerName := strings.Join([]string{appname, entrypoint, suffix}, "_")
-
-	config := info.Config
-	config.Image = image
-
-	hostConfig := info.HostConfig
-	networkConfig := &enginenetwork.NetworkingConfig{
-		EndpointsConfig: info.NetworkSettings.Networks,
-	}
-	return config, hostConfig, networkConfig, containerName, nil
-}
-
-// see https://github.com/docker/docker/issues/6705
-// docker's stupid problem
-func trimLeftSlash(name string) string {
-	return strings.TrimPrefix(name, "/")
 }
 
 // make mount paths
