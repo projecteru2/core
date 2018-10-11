fc5d100390e130528baf84c8330cc512750e12fb
CMGS <ilskdw@gmail.com>
2018-10-11 15:57:11 +0800

    clean container if it had no id

--- cluster/calcium/create_container.go
+++ cluster/calcium/create_container.go
@@ -394,12 +394,12 @@ func (c *Calcium) createAndStartContainer(
 	}
 
 	defer func() {
-		if !createContainerMessage.Success {
+		if !createContainerMessage.Success && container.ID != "" {
 			if err := c.doRemoveContainer(ctx, container); err != nil {
 				log.Errorf("[createAndStartContainer] create and start container failed, and remove it failed also %v", err)
-			} else {
-				createContainerMessage.ContainerID = ""
+				return
 			}
+			createContainerMessage.ContainerID = ""
 		}
 	}()
 
