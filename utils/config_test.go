5748ae8457f6079767b7802f653ec3f54c5d3fb9
CMGS <ilskdw@gmail.com>
2018-11-12 17:43:51 +0800

    add tests for utils

---
+++ utils/config_test.go
@@ -0,0 +1,63 @@
+package utils
+
+import (
+	"bytes"
+	"io/ioutil"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLoadConfig(t *testing.T) {
+	f1 := "test"
+	buffer := bytes.NewBufferString(f1)
+	fname, err := TempFile(ioutil.NopCloser(buffer))
+	assert.NoError(t, err)
+	_, err = LoadConfig(fname)
+	assert.Error(t, err)
+	os.Remove(fname)
+
+	f1 = `log_level: "DEBUG"
+bind: ":5001"
+statsd: "127.0.0.1:8125"
+image_cache: 2
+profile: ":12346"
+global_timeout: 300
+
+auth:
+    username: admin
+    password: password
+etcd:
+    machines:
+        - "http://127.0.0.1:2379"
+    lock_prefix: "core/_lock"
+git:
+    public_key: "***REMOVED***"
+    private_key: "***REMOVED***"
+    token: "***REMOVED***"
+    scm_type: "github"
+docker:
+    network_mode: "bridge"
+    cert_path: "/etc/eru/tls"
+    hub: "hub.docker.com"
+    namespace: "projecteru2"
+    build_pod: "eru-test"
+    local_dns: true
+`
+
+	buffer = bytes.NewBufferString(f1)
+	fname, err = TempFile(ioutil.NopCloser(buffer))
+	assert.NoError(t, err)
+	config, err := LoadConfig(fname)
+	assert.NoError(t, err)
+	assert.Equal(t, config.LockTimeout, defaultTTL)
+	assert.Equal(t, config.GlobalTimeout, time.Duration(time.Second*300))
+	assert.Equal(t, config.Etcd.Prefix, defaultPrefix)
+	assert.Equal(t, config.Docker.Log.Type, "journald")
+	assert.Equal(t, config.Docker.APIVersion, "1.32")
+	assert.Equal(t, config.Scheduler.MaxShare, -1)
+	assert.Equal(t, config.Scheduler.ShareBase, 100)
+	os.Remove(fname)
+}
--- utils/utils_test.go
+++ utils/utils_test.go
@@ -1,9 +1,20 @@
 package utils
 
 import (
+	"archive/tar"
+	"bytes"
+	"context"
 	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
 	"testing"
 
+	"github.com/docker/docker/api/types/network"
+	engineapi "github.com/docker/docker/client"
+	"github.com/docker/docker/pkg/stdcopy"
+	"github.com/projecteru2/core/cluster"
+	"github.com/projecteru2/core/types"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -13,13 +24,6 @@ func TestRandomString(t *testing.T) {
 	s2 := RandomString(10)
 	assert.Equal(t, 10, len(s2))
 	assert.NotEqual(t, s1, s2, fmt.Sprintf("s1: %s, s2: %s", s1, s2))
-}
-
-func TestTruncateID(t *testing.T) {
-	r1 := TruncateID("1234")
-	assert.Equal(t, r1, "1234")
-	r2 := TruncateID("12345678")
-	assert.Equal(t, r2, "1234567")
 }
 
 func TestTail(t *testing.T) {
@@ -33,6 +37,21 @@ func TestTail(t *testing.T) {
 	assert.Equal(t, r4, "c")
 }
 
+func TestFuckDockerStream(t *testing.T) {
+	startingBufLen := 3
+	stdOutBytes := []byte(strings.Repeat("o", startingBufLen))
+	stdErrBytes := []byte(strings.Repeat("e", startingBufLen))
+	buffer := new(bytes.Buffer)
+	dstOut := stdcopy.NewStdWriter(buffer, stdcopy.Stdout)
+	dstOut.Write(stdOutBytes)
+	dstErr := stdcopy.NewStdWriter(buffer, stdcopy.Stderr)
+	dstErr.Write(stdErrBytes)
+
+	outr := FuckDockerStream(ioutil.NopCloser(buffer))
+	b, _ := ioutil.ReadAll(outr)
+	assert.Equal(t, string(b), "oooeee")
+}
+
 func TestGetGitRepoName(t *testing.T) {
 	_, err := GetGitRepoName("xxx")
 	assert.Error(t, err)
@@ -43,6 +62,31 @@ func TestGetGitRepoName(t *testing.T) {
 	r1, err := GetGitRepoName("git@github.com:projecteru2/core.git")
 	assert.NoError(t, err)
 	assert.Equal(t, r1, "core")
+}
+
+func TestGetTag(t *testing.T) {
+	v := GetTag("xx")
+	assert.Equal(t, v, DefaultVersion)
+	v = GetTag("xx:1:2")
+	assert.Equal(t, v, WrongVersion)
+	v = GetTag("xx:2")
+	assert.Equal(t, v, "2")
+}
+
+func TestNormalizeImageName(t *testing.T) {
+	i := NormalizeImageName("image")
+	assert.Equal(t, i, "image:latest")
+	i = NormalizeImageName("image:1")
+	assert.Equal(t, i, "image:1")
+}
+
+func TestGetDockerEngineFromContext(t *testing.T) {
+	ctx := context.Background()
+	engine := &engineapi.Client{}
+	ctx = ContextWithDockerEngine(ctx, engine)
+	client, ok := GetDockerEngineFromContext(ctx)
+	assert.Equal(t, ok, true)
+	assert.Equal(t, client, engine)
 }
 
 func TestMakeCommandLine(t *testing.T) {
@@ -60,4 +104,101 @@ func TestMakeContainerName(t *testing.T) {
 	assert.Equal(t, entrypoint, "web")
 	assert.Equal(t, ident, "whatever")
 	assert.Equal(t, err, nil)
+}
+
+func TestParseContainerName(t *testing.T) {
+	appname := "test_bad"
+	p1, p2, p3, err := ParseContainerName(appname)
+	assert.Error(t, err)
+	assert.Equal(t, p1, "")
+	assert.Equal(t, p2, "")
+	assert.Equal(t, p3, "")
+	appname = "test_good_name_1"
+	p1, p2, p3, err = ParseContainerName(appname)
+	assert.NoError(t, err)
+	assert.Equal(t, p1, "test_good")
+	assert.Equal(t, p2, "name")
+	assert.Equal(t, p3, "1")
+}
+
+func TestPublishInfo(t *testing.T) {
+	node := &types.Node{Endpoint: "tcp://127.0.0.1:12345"}
+	ports := []string{"123", "233"}
+	n1 := map[string]*network.EndpointSettings{
+		"n1": &network.EndpointSettings{
+			IPAddress: "233.233.233.233",
+		},
+		"host": &network.EndpointSettings{},
+	}
+	r := MakePublishInfo(n1, node, ports)
+	assert.Equal(t, len(r), 2)
+	assert.Equal(t, len(r["n1"]), 2)
+	assert.Equal(t, r["n1"][0], "233.233.233.233:123")
+	assert.Equal(t, r["n1"][1], "233.233.233.233:233")
+	assert.Equal(t, len(r["host"]), 2)
+	assert.Equal(t, r["host"][0], "127.0.0.1:123")
+	assert.Equal(t, r["host"][1], "127.0.0.1:233")
+
+	e := EncodePublishInfo(r)
+	assert.Equal(t, len(e), 2)
+	assert.Equal(t, e["n1"], "233.233.233.233:123,233.233.233.233:233")
+	assert.Equal(t, e["host"], "127.0.0.1:123,127.0.0.1:233")
+
+	r2 := DecodePublishInfo(e)
+	assert.Equal(t, len(r2), 2)
+	assert.Equal(t, len(r2["n1"]), 2)
+	assert.Equal(t, len(r2["host"]), 2)
+	assert.Equal(t, r2["n1"][0], "233.233.233.233:123")
+	assert.Equal(t, r2["n1"][1], "233.233.233.233:233")
+	assert.Equal(t, r2["host"][0], "127.0.0.1:123")
+	assert.Equal(t, r2["host"][1], "127.0.0.1:233")
+}
+
+func TestMetaInLabel(t *testing.T) {
+	meta := &types.EruMeta{
+		Publish: []string{"1", "2"},
+	}
+	r := EncodeMetaInLabel(meta)
+	assert.NotEmpty(t, r)
+
+	labels := map[string]string{
+		cluster.ERUMeta: "{\"Publish\":[\"5001\"],\"HealthCheck\":{\"TCPPorts\":[\"5001\"],\"HTTPPort\":\"\",\"HTTPURL\":\"\",\"HTTPCode\":0}}",
+	}
+	meta2 := DecodeMetaInLabel(labels)
+	assert.Equal(t, meta2.Publish[0], "5001")
+}
+
+func TestShortID(t *testing.T) {
+	r1 := ShortID("1234")
+	assert.Equal(t, r1, "1234")
+	r2 := ShortID("12345678")
+	assert.Equal(t, r2, "1234567")
+}
+
+func TestFilterContainer(t *testing.T) {
+	e := map[string]string{"a": "b"}
+	l := map[string]string{"a": "b"}
+	assert.True(t, FilterContainer(e, l))
+	l["c"] = "d"
+	assert.False(t, FilterContainer(e, l))
+}
+
+func TestCleanStatsdMetrics(t *testing.T) {
+	k := "a.b.c"
+	assert.Equal(t, CleanStatsdMetrics(k), "a-b-c")
+}
+
+func TestTempTarFile(t *testing.T) {
+	data := []byte("test")
+	path := "/tmp/test"
+	fname, err := TempTarFile(path, data)
+	assert.NoError(t, err)
+	f, err := os.Open(fname)
+	assert.NoError(t, err)
+	tr := tar.NewReader(f)
+	hdr, err := tr.Next()
+	assert.NoError(t, err)
+	assert.Equal(t, hdr.Name, "test")
+	f.Close()
+	os.Remove(fname)
 }
