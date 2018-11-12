02a48b5db1b83b94effab5e4baedd8cdde01e2ba
CMGS <ilskdw@gmail.com>
2018-11-12 11:08:39 +0800

    hotfix deploy first container

--- store/etcdv3/deploy.go
+++ store/etcdv3/deploy.go
@@ -9,24 +9,24 @@ import (
 
 	"github.com/coreos/etcd/clientv3"
 	"github.com/projecteru2/core/types"
+	log "github.com/sirupsen/logrus"
 )
 
 // MakeDeployStatus get deploy status from store
 func (m *Mercury) MakeDeployStatus(ctx context.Context, opts *types.DeployOptions, nodesInfo []types.NodeInfo) ([]types.NodeInfo, error) {
 	key := filepath.Join(containerDeployPrefix, opts.Name, opts.Entrypoint.Name)
 	resp, err := m.Get(ctx, key, clientv3.WithPrefix(), clientv3.WithKeysOnly())
-	if err != nil || resp.Count == 0 {
-		if resp != nil && resp.Count == 0 {
-			err = types.ErrKeyNotExists
-		}
-		return nil, err
-	}
-
-	nodesInfo, err = m.doGetDeployStatus(ctx, resp, nodesInfo)
 	if err != nil {
 		return nil, err
 	}
-
+	if resp.Count != 0 {
+		nodesInfo, err = m.doGetDeployStatus(ctx, resp, nodesInfo)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		log.Warnf("[MakeDeployStatus] Deploy status not found %s", opts.Name)
+	}
 	return m.loadProcessing(ctx, opts, nodesInfo)
 }
 
