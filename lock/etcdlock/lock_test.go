bf2d147e1c3260989df1a4b689a951ceba04f89f
CMGS <ilskdw@gmail.com>
2018-11-15 11:21:03 +0800

    add etcd lock tests

---
+++ lock/etcdlock/lock_test.go
@@ -0,0 +1,27 @@
+package etcdlock
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/coreos/etcd/integration"
+)
+
+func TestLock(t *testing.T) {
+	cluster := integration.NewClusterV3(t, &integration.ClusterConfig{Size: 1})
+	defer cluster.Terminate(t)
+	cli := cluster.RandClient()
+
+	_, err := New(cli, "", 1)
+	assert.Error(t, err)
+	mutex, err := New(cli, "test", 1)
+	assert.NoError(t, err)
+
+	ctx := context.Background()
+	err = mutex.Lock(ctx)
+	assert.NoError(t, err)
+	err = mutex.Unlock(ctx)
+	assert.NoError(t, err)
+}
