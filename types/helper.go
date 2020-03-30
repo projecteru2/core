225aa0ce70b07a682c74f55b792799b34cd788a3
zc <zhichuan.liang@shopee.com>
2020-03-30 15:10:21 +0800

    expand resource calculation precision (#191)
    * expand resource calculation precision
    
    * fix unittest

--- types/helper.go
+++ types/helper.go
@@ -7,7 +7,7 @@ import (
 
 // Round for float64 to int
 func Round(f float64) float64 {
-	return math.Round(f*100) / 100
+	return math.Round(f*1000000) / 1000000
 }
 
 // HookOutput output hooks output
