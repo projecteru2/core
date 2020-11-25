c3066540bda68b4bfa0403ffc14d1c3a0581befa
雾雨 <isayama.yomi@outlook.com>
2020-11-25 10:15:15 +0800

    add unit-test for helium and resources (#266)
    fix code style issues
    
    Co-authored-by: nyanpassu <yumeko@outlook.com>

---
+++ resources/volume/volume_test.go
@@ -0,0 +1,155 @@
+package volume
+
+import (
+	"testing"
+
+	resourcetypes "github.com/projecteru2/core/resources/types"
+	"github.com/projecteru2/core/scheduler"
+	schedulerMocks "github.com/projecteru2/core/scheduler/mocks"
+	"github.com/projecteru2/core/types"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestMakeRequest(t *testing.T) {
+	_, err := MakeRequest(types.ResourceOptions{
+		VolumeLimit: []*types.VolumeBinding{
+			&types.VolumeBinding{
+				Source:      "/data1",
+				Destination: "/data1",
+			},
+		},
+	})
+	assert.Nil(t, err)
+
+	_, err = MakeRequest(types.ResourceOptions{
+		VolumeRequest: []*types.VolumeBinding{
+			&types.VolumeBinding{
+				Source:      "/data1",
+				Destination: "/data1",
+			},
+		},
+		VolumeLimit: []*types.VolumeBinding{
+			&types.VolumeBinding{
+				Source:      "/data1",
+				Destination: "/data1",
+			},
+			&types.VolumeBinding{
+				Source:      "/data2",
+				Destination: "/data2",
+			},
+		},
+	})
+	assert.NotNil(t, err)
+
+}
+
+func TestStorage(t *testing.T) {
+	mockScheduler := &schedulerMocks.Scheduler{}
+	var (
+		volumePlans = []types.VolumePlan{
+			{
+				types.VolumeBinding{
+					Source:      "/data1",
+					Destination: "/data1",
+				}: types.VolumeMap{
+					"/data1": 512,
+				},
+			},
+		}
+		nodeInfos []types.NodeInfo = []types.NodeInfo{
+			{
+				Name:       "TestNode",
+				CPUMap:     map[string]int64{"0": 10000, "1": 10000},
+				NUMA:       map[string]string{"0": "0", "1": "1"},
+				NUMAMemory: map[string]int64{"0": 1024, "1": 1204},
+				MemCap:     10240,
+				CPUPlan:    []types.CPUMap{{"0": 10000, "1": 10000}},
+				StorageCap: 10240,
+				VolumeMap: types.VolumeMap{
+					"/data1": 1024,
+					"/data2": 1024,
+				},
+				InitVolumeMap: types.VolumeMap{
+					"/data0": 1024,
+				},
+				VolumePlans: volumePlans,
+			},
+		}
+		volumePlan = map[string][]types.VolumePlan{
+			"TestNode": volumePlans,
+		}
+	)
+	mockScheduler.On(
+		"SelectVolumeNodes", mock.Anything, mock.Anything,
+	).Return(nodeInfos, volumePlan, 1, nil)
+
+	prevSche, _ := scheduler.GetSchedulerV1()
+	scheduler.InitSchedulerV1(mockScheduler)
+	defer func() {
+		scheduler.InitSchedulerV1(prevSche)
+	}()
+
+	resourceRequest, err := MakeRequest(types.ResourceOptions{
+		VolumeRequest: []*types.VolumeBinding{
+			&types.VolumeBinding{
+				Source:      "/data1",
+				Destination: "/data1",
+			},
+		},
+		VolumeLimit: []*types.VolumeBinding{
+			&types.VolumeBinding{
+				Source:      "/data1",
+				Destination: "/data1",
+			},
+		},
+	})
+	assert.Nil(t, err)
+	assert.True(t, resourceRequest.Type()&types.ResourceVolume > 0)
+
+	sche := resourceRequest.MakeScheduler()
+
+	plans, _, err := sche(nodeInfos)
+	assert.Nil(t, err)
+	assert.True(t, plans.Type()&types.ResourceVolume > 0)
+
+	const storage = int64(10240)
+	var node = types.Node{
+		Name:       "TestNode",
+		CPU:        map[string]int64{"0": 10000, "1": 10000},
+		NUMA:       map[string]string{"0": "0", "1": "1"},
+		NUMAMemory: map[string]int64{"0": 1024, "1": 1204},
+		MemCap:     10240,
+		StorageCap: storage,
+		Volume:     types.VolumeMap{"/data1": 512, "/data2": 512},
+		VolumeUsed: 0,
+		InitVolume: types.VolumeMap{"/data1": 512, "/data2": 512},
+	}
+
+	plans.ApplyChangesOnNode(&node, 0)
+	assert.Less(t, node.Volume["/data1"], int64(512))
+
+	plans.RollbackChangesOnNode(&node, 0)
+	assert.Equal(t, node.Volume["/data1"], int64(512))
+
+	opts := resourcetypes.DispenseOptions{
+		Node:  &node,
+		Index: 0,
+		ExistingInstance: &types.Workload{
+			ResourceMeta: types.ResourceMeta{
+				VolumePlanRequest: types.VolumePlan{
+					types.VolumeBinding{
+						Source:      "/data1",
+						Destination: "/data1",
+					}: types.VolumeMap{
+						"/data1": 512,
+					},
+				},
+			},
+		},
+	}
+	r := &types.ResourceMeta{}
+	_, err = plans.Dispense(opts, r)
+	assert.Nil(t, err)
+}
