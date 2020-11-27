a310b56301295ff8d8a0ecf394e43853a6915dbd
雾雨 <isayama.yomi@outlook.com>
2020-11-27 10:33:13 +0800

    tests: adding test for volume (#285)
    Co-authored-by: nyanpassu <yumeko@outlook.com>

--- resources/volume/volume_test.go
+++ resources/volume/volume_test.go
@@ -23,6 +23,78 @@ func TestMakeRequest(t *testing.T) {
 	})
 	assert.Nil(t, err)
 
+	// Source not match
+	_, err = MakeRequest(types.ResourceOptions{
+		VolumeRequest: []*types.VolumeBinding{
+			{
+				Source:      "/data1",
+				Destination: "/data1",
+			},
+		},
+		VolumeLimit: []*types.VolumeBinding{
+			{
+				Source:      "/data2",
+				Destination: "/data1",
+			},
+		},
+	})
+	assert.NotNil(t, err)
+
+	// Dest not match
+	_, err = MakeRequest(types.ResourceOptions{
+		VolumeRequest: []*types.VolumeBinding{
+			{
+				Source:      "/data1",
+				Destination: "/data1",
+			},
+		},
+		VolumeLimit: []*types.VolumeBinding{
+			{
+				Source:      "/data1",
+				Destination: "/data2",
+			},
+		},
+	})
+	assert.NotNil(t, err)
+
+	// Flag not match
+	_, err = MakeRequest(types.ResourceOptions{
+		VolumeRequest: []*types.VolumeBinding{
+			{
+				Source:      "/data1",
+				Destination: "/data1",
+				Flags:       "r",
+			},
+		},
+		VolumeLimit: []*types.VolumeBinding{
+			{
+				Source:      "/data1",
+				Destination: "/data1",
+				Flags:       "rw",
+			},
+		},
+	})
+	assert.NotNil(t, err)
+
+	// Request SizeInBytes larger then limit
+	_, err = MakeRequest(types.ResourceOptions{
+		VolumeRequest: []*types.VolumeBinding{
+			{
+				Source:      "/data1",
+				Destination: "/data1",
+				SizeInBytes: 10240,
+			},
+		},
+		VolumeLimit: []*types.VolumeBinding{
+			{
+				Source:      "/data1",
+				Destination: "/data1",
+				SizeInBytes: 5120,
+			},
+		},
+	})
+	assert.NoError(t, err)
+
 	_, err = MakeRequest(types.ResourceOptions{
 		VolumeRequest: []*types.VolumeBinding{
 			{
@@ -43,59 +115,19 @@ func TestMakeRequest(t *testing.T) {
 	})
 	assert.NotNil(t, err)
 
-}
-
-func TestStorage(t *testing.T) {
-	mockScheduler := &schedulerMocks.Scheduler{}
-	var (
-		volumePlans = []types.VolumePlan{
-			{
-				types.VolumeBinding{
-					Source:      "/data1",
-					Destination: "/data1",
-				}: types.VolumeMap{
-					"/data1": 512,
-				},
-			},
-		}
-		nodeInfos []types.NodeInfo = []types.NodeInfo{
-			{
-				Name:       "TestNode",
-				CPUMap:     map[string]int64{"0": 10000, "1": 10000},
-				NUMA:       map[string]string{"0": "0", "1": "1"},
-				NUMAMemory: map[string]int64{"0": 1024, "1": 1204},
-				MemCap:     10240,
-				CPUPlan:    []types.CPUMap{{"0": 10000, "1": 10000}},
-				StorageCap: 10240,
-				VolumeMap: types.VolumeMap{
-					"/data1": 1024,
-					"/data2": 1024,
-				},
-				InitVolumeMap: types.VolumeMap{
-					"/data0": 1024,
-				},
-				VolumePlans: volumePlans,
-			},
-		}
-		volumePlan = map[string][]types.VolumePlan{
-			"TestNode": volumePlans,
-		}
-	)
-	mockScheduler.On(
-		"SelectVolumeNodes", mock.Anything, mock.Anything,
-	).Return(nodeInfos, volumePlan, 1, nil)
-
-	prevSche, _ := scheduler.GetSchedulerV1()
-	scheduler.InitSchedulerV1(mockScheduler)
-	defer func() {
-		scheduler.InitSchedulerV1(prevSche)
-	}()
-
-	resourceRequest, err := MakeRequest(types.ResourceOptions{
+	_, err = MakeRequest(types.ResourceOptions{
 		VolumeRequest: []*types.VolumeBinding{
 			{
 				Source:      "/data1",
 				Destination: "/data1",
+			},
+			{
+				Source:      "/data3",
+				Destination: "/data3",
+			},
+			{
+				Source:      "/data2",
+				Destination: "/data2",
 			},
 		},
 		VolumeLimit: []*types.VolumeBinding{
@@ -103,14 +135,135 @@ func TestStorage(t *testing.T) {
 				Source:      "/data1",
 				Destination: "/data1",
 			},
+			{
+				Source:      "/data4",
+				Destination: "/data4",
+			},
+		},
+	})
+	assert.NotNil(t, err)
+
+}
+
+func TestType(t *testing.T) {
+	resourceRequest, err := MakeRequest(types.ResourceOptions{
+		VolumeRequest: []*types.VolumeBinding{
+			{
+				Source:      "AUTO",
+				Destination: "/data1",
+				Flags:       "rw",
+				SizeInBytes: 128,
+			},
+		},
+		VolumeLimit: []*types.VolumeBinding{
+			{
+				Source:      "AUTO",
+				Destination: "/data1",
+				Flags:       "rw",
+				SizeInBytes: 128,
+			},
 		},
 	})
 	assert.Nil(t, err)
-	assert.True(t, resourceRequest.Type()&types.ResourceVolume > 0)
+	assert.True(t, resourceRequest.Type()&(types.ResourceVolume|types.ResourceScheduledVolume) > 0)
+}
 
+func TestStoragePlans(t *testing.T) {
+	testStoragePlans(t, types.ResourceOptions{
+		VolumeRequest: []*types.VolumeBinding{
+			{
+				Source:      "AUTO",
+				Destination: "/data1",
+				Flags:       "rw",
+				SizeInBytes: 128,
+			},
+		},
+		VolumeLimit: []*types.VolumeBinding{
+			{
+				Source:      "AUTO",
+				Destination: "/data1",
+				Flags:       "rw",
+				SizeInBytes: 256,
+			},
+		},
+	})
+	testStoragePlans(t, types.ResourceOptions{
+		VolumeRequest: []*types.VolumeBinding{
+			{
+				Source:      "AUTO",
+				Destination: "/data1",
+				Flags:       "rw",
+				SizeInBytes: 128,
+			},
+		},
+		VolumeLimit: []*types.VolumeBinding{
+			{
+				Source:      "AUTO",
+				Destination: "/data1",
+				Flags:       "rw",
+				SizeInBytes: 128,
+			},
+		},
+	})
+}
+
+func testStoragePlans(t *testing.T, reqOpts types.ResourceOptions) {
+	mockScheduler := &schedulerMocks.Scheduler{}
+	var (
+		volumePlans = []types.VolumePlan{
+			{
+				types.VolumeBinding{
+					Source:      "AUTO",
+					Destination: "/data1",
+					Flags:       "rw",
+					SizeInBytes: 128,
+				}: types.VolumeMap{
+					"/dev0": 512,
+				},
+			},
+			{
+				types.VolumeBinding{
+					Source:      "AUTO",
+					Destination: "/data1",
+					Flags:       "rw",
+					SizeInBytes: 128,
+				}: types.VolumeMap{
+					"/dev1": 512,
+				},
+			},
+		}
+		nodeInfos []types.NodeInfo = []types.NodeInfo{
+			{
+				Name:     "TestNode",
+				Capacity: 100,
+			},
+		}
+		volumePlan = map[string][]types.VolumePlan{
+			"TestNode": volumePlans,
+		}
+	)
+
+	resourceRequest, err := MakeRequest(reqOpts)
+	assert.Nil(t, err)
+	assert.True(t, resourceRequest.Type()&types.ResourceVolume > 0)
 	sche := resourceRequest.MakeScheduler()
 
+	mockScheduler.On(
+		"SelectVolumeNodes", mock.Anything, mock.Anything,
+	).Return(nodeInfos, volumePlan, 1, nil)
+
+	prevSche, _ := scheduler.GetSchedulerV1()
+	scheduler.InitSchedulerV1(nil)
+
 	plans, _, err := sche(nodeInfos)
+	assert.Error(t, err)
+
+	scheduler.InitSchedulerV1(mockScheduler)
+	defer func() {
+		scheduler.InitSchedulerV1(prevSche)
+	}()
+
+	plans, _, err = sche(nodeInfos)
 	assert.Nil(t, err)
 	assert.True(t, plans.Type()&types.ResourceVolume > 0)
 
@@ -122,19 +275,132 @@ func TestStorage(t *testing.T) {
 		NUMAMemory: map[string]int64{"0": 1024, "1": 1204},
 		MemCap:     10240,
 		StorageCap: storage,
-		Volume:     types.VolumeMap{"/data1": 512, "/data2": 512},
+		Volume:     types.VolumeMap{"/dev0": 10240, "/dev1": 5120},
 		VolumeUsed: 0,
-		InitVolume: types.VolumeMap{"/data1": 512, "/data2": 512},
+	}
+
+	assert.NotNil(t, plans.Capacity())
+	plans.ApplyChangesOnNode(&node, 0, 1)
+	assert.Less(t, node.Volume["/dev0"], int64(10240))
+	assert.Less(t, node.Volume["/dev1"], int64(5120))
+
+	plans.RollbackChangesOnNode(&node, 0, 1)
+	assert.Equal(t, node.Volume["/dev0"], int64(10240))
+	assert.Equal(t, node.Volume["/dev1"], int64(5120))
+
+	opts := resourcetypes.DispenseOptions{
+		Node:  &node,
+		Index: 0,
+	}
+	r := &types.ResourceMeta{}
+	_, err = plans.Dispense(opts, r)
+	assert.Nil(t, err)
+
+	if reqOpts.VolumeRequest[0].SizeInBytes != reqOpts.VolumeLimit[0].SizeInBytes {
+		diff := reqOpts.VolumeLimit[0].SizeInBytes - reqOpts.VolumeRequest[0].SizeInBytes
+		assert.Equal(t, int64(512)+diff, r.VolumePlanLimit[*reqOpts.VolumeLimit[0]]["/dev0"])
+		return
+	}
+	assert.Equal(t, int64(512), r.VolumePlanLimit[*reqOpts.VolumeRequest[0]]["/dev0"])
+}
+
+func TestStorage(t *testing.T) {
+	mockScheduler := &schedulerMocks.Scheduler{}
+	var (
+		volumePlans = []types.VolumePlan{
+			{
+				types.VolumeBinding{
+					Source:      "/data1",
+					Destination: "/data1",
+					Flags:       "rw",
+					SizeInBytes: 128,
+				}: types.VolumeMap{
+					"/dev0": 512,
+				},
+			},
+		}
+		nodeInfos []types.NodeInfo = []types.NodeInfo{
+			{
+				Name:     "TestNode",
+				Capacity: 100,
+			},
+		}
+		volumePlan = map[string][]types.VolumePlan{
+			"TestNode": volumePlans,
+		}
+	)
+
+	resourceRequest, err := MakeRequest(types.ResourceOptions{
+		VolumeRequest: []*types.VolumeBinding{
+			{
+				Source:      "/data1",
+				Destination: "/data1",
+				Flags:       "rw",
+				SizeInBytes: 128,
+			},
+		},
+		VolumeLimit: []*types.VolumeBinding{
+			{
+				Source:      "/data1",
+				Destination: "/data1",
+				Flags:       "rw",
+				SizeInBytes: 128,
+			},
+		},
+	})
+	assert.Nil(t, err)
+	assert.True(t, resourceRequest.Type()&types.ResourceVolume > 0)
+	sche := resourceRequest.MakeScheduler()
+
+	mockScheduler.On(
+		"SelectVolumeNodes", mock.Anything, mock.Anything,
+	).Return(nodeInfos, volumePlan, 1, nil)
+
+	prevSche, _ := scheduler.GetSchedulerV1()
+	scheduler.InitSchedulerV1(nil)
+
+	plans, _, err := sche(nodeInfos)
+	assert.Error(t, err)
+
+	scheduler.InitSchedulerV1(mockScheduler)
+	defer func() {
+		scheduler.InitSchedulerV1(prevSche)
+	}()
+
+	plans, _, err = sche(nodeInfos)
+	assert.Nil(t, err)
+	assert.True(t, plans.Type()&types.ResourceVolume > 0)
+
+	const storage = int64(10240)
+	var node = types.Node{
+		Name:       "TestNode",
+		CPU:        map[string]int64{"0": 10000, "1": 10000},
+		NUMA:       map[string]string{"0": "0", "1": "1"},
+		NUMAMemory: map[string]int64{"0": 1024, "1": 1204},
+		MemCap:     10240,
+		StorageCap: storage,
+		Volume:     types.VolumeMap{"/dev0": 10240, "/dev1": 5120},
+		VolumeUsed: 0,
 	}
 
 	assert.NotNil(t, plans.Capacity())
 	plans.ApplyChangesOnNode(&node, 0)
-	assert.Less(t, node.Volume["/data1"], int64(512))
+	assert.Less(t, node.Volume["/dev0"], int64(10240))
+	assert.Equal(t, node.Volume["/dev1"], int64(5120))
 
 	plans.RollbackChangesOnNode(&node, 0)
-	assert.Equal(t, node.Volume["/data1"], int64(512))
+	assert.Equal(t, node.Volume["/dev0"], int64(10240))
+	assert.Equal(t, node.Volume["/dev1"], int64(5120))
 
 	opts := resourcetypes.DispenseOptions{
+		Node:  &node,
+		Index: 0,
+	}
+	r := &types.ResourceMeta{}
+	_, err = plans.Dispense(opts, r)
+	assert.Nil(t, err)
+
+	opts = resourcetypes.DispenseOptions{
 		Node:  &node,
 		Index: 0,
 		ExistingInstance: &types.Workload{
@@ -144,15 +410,35 @@ func TestStorage(t *testing.T) {
 						Source:      "/data1",
 						Destination: "/data1",
 					}: types.VolumeMap{
-						"/data1": 512,
+						"/dev0": 5120,
 					},
 				},
 			},
 		},
 	}
-	r := &types.ResourceMeta{}
+	r = &types.ResourceMeta{}
 	_, err = plans.Dispense(opts, r)
 	assert.Nil(t, err)
+
+	opts = resourcetypes.DispenseOptions{
+		Node:  &node,
+		Index: 0,
+		ExistingInstance: &types.Workload{
+			ResourceMeta: types.ResourceMeta{
+				VolumePlanRequest: types.VolumePlan{
+					types.VolumeBinding{
+						Source:      "/data1",
+						Destination: "/data1",
+					}: types.VolumeMap{
+						"/data3": 512,
+					},
+				},
+			},
+		},
+	}
+	r = &types.ResourceMeta{}
+	_, err = plans.Dispense(opts, r)
+	assert.Error(t, err)
 }
 
 func TestRate(t *testing.T) {
