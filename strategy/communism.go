753aa934881aef04edf26fd6d0b6639d2d2a5f53
zc <greyschwinger@gmail.com>
2021-01-08 23:23:24 +0800

    New Communisim Strategy (#313)
    * new communisim: heap based
    
    * eliminate strategy side effects
    
    * add more tests for communism

--- strategy/communism.go
+++ strategy/communism.go
@@ -1,57 +1,82 @@
 package strategy
 
 import (
+	"container/heap"
 	"fmt"
-	"sort"
 
 	"github.com/pkg/errors"
-	"github.com/projecteru2/core/log"
 	"github.com/projecteru2/core/types"
 )
 
+type infoHeap struct {
+	infos []Info
+	limit int
+}
+
+func (h infoHeap) Len() int {
+	return len(h.infos)
+}
+
+func (h infoHeap) Less(i, j int) bool {
+	return h.infos[i].Count < h.infos[j].Count || (h.infos[i].Count == h.infos[j].Count && h.infos[i].Capacity > h.infos[j].Capacity)
+}
+
+func (h infoHeap) Swap(i, j int) {
+	h.infos[i], h.infos[j] = h.infos[j], h.infos[i]
+}
+
+func (h *infoHeap) Push(x interface{}) {
+	info := x.(Info)
+	if info.Capacity == 0 || (h.limit > 0 && info.Count >= h.limit) {
+		return
+	}
+	h.infos = append(h.infos, info)
+}
+
+func (h *infoHeap) Pop() interface{} {
+	length := len(h.infos)
+	x := h.infos[length-1]
+	h.infos = h.infos[0 : length-1]
+	return x
+}
+
+func newInfoHeap(infos []Info, limit int) heap.Interface {
+	dup := infoHeap{
+		infos: []Info{},
+		limit: limit,
+	}
+	for _, info := range infos {
+		if info.Capacity == 0 || (limit > 0 && info.Count >= limit) {
+			continue
+		}
+		dup.infos = append(dup.infos, info)
+	}
+	return &dup
+}
+
 // CommunismPlan 吃我一记共产主义大锅饭
 // 部署完 N 个后全局尽可能平均
-func CommunismPlan(arg []Info, need, total, limit int, resourceType types.ResourceType) (map[string]int, error) {
+func CommunismPlan(infos []Info, need, total, limit int, resourceType types.ResourceType) (map[string]int, error) {
 	if total < need {
 		return nil, errors.WithStack(types.NewDetailedErr(types.ErrInsufficientRes,
 			fmt.Sprintf("need: %d, vol: %d", need, total)))
 	}
-	sort.Slice(arg, func(i, j int) bool { return arg[i].Count < arg[j].Count })
-	length := len(arg)
 
-	deployMap := map[string]int{}
-	for i := 0; i < length; i++ {
-		if need <= 0 {
-			break
+	deploy := map[string]int{}
+	iHeap := newInfoHeap(infos, limit)
+	heap.Init(iHeap)
+	for {
+		if iHeap.Len() == 0 {
+			return nil, errors.WithStack(types.ErrInsufficientRes)
 		}
-		req := need
-		if i < length-1 {
-			req = (arg[i+1].Count - arg[i].Count) * (i + 1)
+		info := heap.Pop(iHeap).(Info)
+		deploy[info.Nodename]++
+		need--
+		if need == 0 {
+			return deploy, nil
 		}
-		if req > need {
-			req = need
-		}
-		for j := 0; j < i+1; j++ {
-			deploy := req / (i + 1 - j)
-			tail := req % (i + 1 - j)
-			d := deploy
-			if tail > 0 {
-				d++
-			}
-			if d > arg[j].Capacity {
-				d = arg[j].Capacity
-			}
-			if d == 0 {
-				continue
-			}
-			arg[j].Capacity -= d
-			deployMap[arg[j].Nodename] += d
-			need -= d
-			req -= d
-		}
+		info.Count++
+		info.Capacity--
+		heap.Push(iHeap, info)
 	}
-	// 这里 need 一定会为 0 出来，因为 volTotal 保证了一定大于 need
-	// 这里并不需要再次排序了，理论上的排序是基于 Count 得到的 Deploy 最终方案
-	log.Debugf("[CommunismPlan] strategyInfo: %+v", arg)
-	return deployMap, nil
 }
--- strategy/communism_test.go
+++ strategy/communism_test.go
@@ -1,7 +1,9 @@
 package strategy
 
 import (
+	"fmt"
 	"math/rand"
+	"sort"
 	"testing"
 
 	resourcetypes "github.com/projecteru2/core/resources/types"
@@ -14,37 +16,48 @@ func TestCommunismPlan(t *testing.T) {
 	nodes := deployedNodes()
 	r, err := CommunismPlan(nodes, 1, 100, 0, types.ResourceAll)
 	assert.NoError(t, err)
-	assert.Equal(t, r[nodes[0].Nodename], 1)
+	counts := []int{r[nodes[0].Nodename] + nodes[0].Count, r[nodes[1].Nodename] + nodes[1].Count, r[nodes[2].Nodename] + nodes[2].Count, r[nodes[3].Nodename] + nodes[3].Count}
+	sort.Ints(counts)
+	assert.ElementsMatch(t, []int{3, 3, 5, 7}, counts)
 	nodes = deployedNodes()
 	r, err = CommunismPlan(nodes, 2, 1, 0, types.ResourceAll)
 	assert.Error(t, err)
 	nodes = deployedNodes()
 	r, err = CommunismPlan(nodes, 2, 100, 0, types.ResourceAll)
 	assert.NoError(t, err)
-	assert.Equal(t, r[nodes[0].Nodename], 2)
+	counts = []int{r[nodes[0].Nodename] + nodes[0].Count, r[nodes[1].Nodename] + nodes[1].Count, r[nodes[2].Nodename] + nodes[2].Count, r[nodes[3].Nodename] + nodes[3].Count}
+	sort.Ints(counts)
+	assert.ElementsMatch(t, []int{3, 4, 5, 7}, counts)
 	nodes = deployedNodes()
 	r, err = CommunismPlan(nodes, 3, 100, 0, types.ResourceAll)
 	assert.NoError(t, err)
-	assert.Equal(t, r[nodes[0].Nodename], 2)
-	assert.Equal(t, r[nodes[1].Nodename], 1)
+	counts = []int{r[nodes[0].Nodename] + nodes[0].Count, r[nodes[1].Nodename] + nodes[1].Count, r[nodes[2].Nodename] + nodes[2].Count, r[nodes[3].Nodename] + nodes[3].Count}
+	sort.Ints(counts)
+	assert.ElementsMatch(t, []int{4, 4, 5, 7}, counts)
 	nodes = deployedNodes()
 	r, err = CommunismPlan(nodes, 4, 100, 0, types.ResourceAll)
 	assert.NoError(t, err)
-	assert.Equal(t, r[nodes[0].Nodename], 3)
-	assert.Equal(t, r[nodes[1].Nodename], 1)
+	counts = []int{r[nodes[0].Nodename] + nodes[0].Count, r[nodes[1].Nodename] + nodes[1].Count, r[nodes[2].Nodename] + nodes[2].Count, r[nodes[3].Nodename] + nodes[3].Count}
+	sort.Ints(counts)
+	assert.ElementsMatch(t, []int{4, 5, 5, 7}, counts)
 	nodes = deployedNodes()
 	r, err = CommunismPlan(nodes, 29, 100, 0, types.ResourceAll)
 	assert.NoError(t, err)
-	assert.Equal(t, r[nodes[0].Nodename], 10)
-	assert.Equal(t, r[nodes[1].Nodename], 9)
-	assert.Equal(t, r[nodes[2].Nodename], 6)
-	assert.Equal(t, r[nodes[3].Nodename], 4)
+	counts = []int{r[nodes[0].Nodename] + nodes[0].Count, r[nodes[1].Nodename] + nodes[1].Count, r[nodes[2].Nodename] + nodes[2].Count, r[nodes[3].Nodename] + nodes[3].Count}
+	sort.Ints(counts)
+	assert.ElementsMatch(t, []int{11, 11, 12, 12}, counts)
 	nodes = deployedNodes()
 	r, err = CommunismPlan(nodes, 37, 100, 0, types.ResourceAll)
 	assert.NoError(t, err)
+	counts = []int{r[nodes[0].Nodename] + nodes[0].Count, r[nodes[1].Nodename] + nodes[1].Count, r[nodes[2].Nodename] + nodes[2].Count, r[nodes[3].Nodename] + nodes[3].Count}
+	sort.Ints(counts)
+	assert.ElementsMatch(t, []int{12, 13, 14, 15}, counts)
 	nodes = deployedNodes()
 	r, err = CommunismPlan(nodes, 40, 100, 0, types.ResourceAll)
 	assert.NoError(t, err)
+	counts = []int{r[nodes[0].Nodename] + nodes[0].Count, r[nodes[1].Nodename] + nodes[1].Count, r[nodes[2].Nodename] + nodes[2].Count, r[nodes[3].Nodename] + nodes[3].Count}
+	sort.Ints(counts)
+	assert.ElementsMatch(t, []int{12, 13, 15, 17}, counts)
 }
 
 func randomDeployStatus(scheduleInfos []resourcetypes.ScheduleInfo, maxDeployed int) (sis []Info) {
@@ -76,4 +89,60 @@ func Benchmark_CommunismPlan(b *testing.B) {
 		b.StopTimer()
 		assert.NoError(b, err)
 	}
+}
+
+func TestCommunismPlanCapacityPriority(t *testing.T) {
+	genNodesByCapCount := func(caps, counts []int) (infos []Info) {
+		for i := range caps {
+			infos = append(infos, Info{
+				Nodename: fmt.Sprintf("%d", i),
+				Capacity: caps[i],
+				Count:    counts[i],
+			})
+		}
+		return
+	}
+	getFinalStatus := func(deploy map[string]int, infos []Info) (counts []int) {
+		for _, info := range infos {
+			counts = append(counts, info.Count+deploy[info.Nodename])
+		}
+		sort.Ints(counts)
+		return
+	}
+
+	nodes := genNodesByCapCount([]int{1, 2, 1, 5, 10}, []int{0, 0, 0, 0, 0})
+	deploy, err := CommunismPlan(nodes, 3, 15, 0, types.ResourceAll)
+	assert.Nil(t, err)
+	assert.ElementsMatch(t, []int{0, 0, 1, 1, 1}, getFinalStatus(deploy, nodes))
+	assert.EqualValues(t, 1, deploy["1"])
+	assert.EqualValues(t, 1, deploy["3"])
+	assert.EqualValues(t, 1, deploy["4"])
+
+	nodes = genNodesByCapCount([]int{10, 4, 4}, []int{1, 1, 10})
+	deploy, err = CommunismPlan(nodes, 5, 100, 0, types.ResourceAll)
+	assert.Nil(t, err)
+	assert.ElementsMatch(t, []int{3, 4, 10}, getFinalStatus(deploy, nodes))
+	assert.EqualValues(t, 3, deploy["0"])
+	assert.EqualValues(t, 2, deploy["1"])
+
+	nodes = genNodesByCapCount([]int{4, 5, 4, 10}, []int{2, 2, 4, 0})
+	deploy, err = CommunismPlan(nodes, 3, 100, 0, types.ResourceAll)
+	assert.Nil(t, err)
+	assert.ElementsMatch(t, []int{2, 2, 3, 4}, getFinalStatus(deploy, nodes))
+	assert.EqualValues(t, 3, deploy["3"])
+
+	nodes = genNodesByCapCount([]int{3, 4, 5, 10}, []int{0, 0, 0, 0})
+	deploy, err = CommunismPlan(nodes, 3, 100, 0, types.ResourceAll)
+	assert.Nil(t, err)
+	assert.ElementsMatch(t, []int{0, 1, 1, 1}, getFinalStatus(deploy, nodes))
+	assert.EqualValues(t, 1, deploy["3"])
+	assert.EqualValues(t, 1, deploy["2"])
+	assert.EqualValues(t, 1, deploy["1"])
+
+	// test limit
+	nodes = genNodesByCapCount([]int{3, 4, 5, 10}, []int{3, 5, 7, 10})
+	deploy, err = CommunismPlan(nodes, 3, 10, 5, types.ResourceAll)
+	assert.EqualError(t, err, "not enough resource")
+	deploy, err = CommunismPlan(nodes, 3, 10, 6, types.ResourceAll)
+	assert.Nil(t, err)
 }
