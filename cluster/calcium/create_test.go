972986a48ac7e4c88ea493c16043f28577c507d4
zc <greyschwinger@gmail.com>
2021-03-22 16:10:55 +0800

    bugfix: doRemoveWorkload isn't for rollback of deploy

--- cluster/calcium/create_test.go
+++ cluster/calcium/create_test.go
@@ -232,7 +232,6 @@ func TestCreateWorkloadTxn(t *testing.T) {
 	engine.On("ImageRemoteDigest", mock.Anything, mock.Anything).Return("", nil)
 	engine.On("VirtualizationCreate", mock.Anything, mock.Anything).Return(nil, errors.Wrap(context.DeadlineExceeded, "VirtualizationCreate")).Twice()
 	engine.On("VirtualizationRemove", mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(nil)
-	store.On("RemoveWorkload", mock.Anything, mock.Anything).Return(nil)
 	store.On("ListNodeWorkloads", mock.Anything, mock.Anything, mock.Anything).Return(nil, types.ErrNoETCD)
 	walCommitted = false
 	ch, err = c.CreateWorkload(ctx, opts)
