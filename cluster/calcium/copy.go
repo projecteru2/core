6647f336d22a472bad9abf2610fb79f02f4d13af
tonic <tonicbupt@gmail.com>
2021-06-21 11:38:26 +0800

    copy doesn't need this lock

--- cluster/calcium/copy.go
+++ cluster/calcium/copy.go
@@ -15,25 +15,31 @@ func (c *Calcium) Copy(ctx context.Context, opts *types.CopyOptions) (chan *type
 	if err := opts.Validate(); err != nil {
 		return nil, logger.Err(ctx, err)
 	}
+
 	ch := make(chan *types.CopyMessage)
 	utils.SentryGo(func() {
 		defer close(ch)
+
 		wg := sync.WaitGroup{}
 		log.Infof(ctx, "[Copy] Copy %d workloads files", len(opts.Targets))
+
 		// workload one by one
 		for id, paths := range opts.Targets {
 			wg.Add(1)
+
 			utils.SentryGo(func(id string, paths []string) func() {
 				return func() {
 					defer wg.Done()
-					if err := c.withWorkloadLocked(ctx, id, func(ctx context.Context, workload *types.Workload) error {
-						for _, path := range paths {
-							resp, name, err := workload.Engine.VirtualizationCopyFrom(ctx, workload.ID, path)
-							ch <- makeCopyMessage(id, name, path, err, resp)
-						}
-						return nil
-					}); err != nil {
+
+					workload, err := c.GetWorkload(ctx, id)
+					if err != nil {
 						ch <- makeCopyMessage(id, "", "", logger.Err(ctx, err), nil)
+						return
+					}
+
+					for _, path := range paths {
+						resp, name, err := workload.Engine.VirtualizationCopyFrom(ctx, workload.ID, path)
+						ch <- makeCopyMessage(id, name, path, err, resp)
 					}
 				}
 			}(id, paths))
