d0a9c3c6417f9d833914d647b873d5a3af44e3cb
zc <greyschwinger@gmail.com>
2021-06-25 17:34:23 +0800

    make batchupdate idempotent (#444)

--- store/etcdv3/workload_test.go
+++ store/etcdv3/workload_test.go
@@ -36,7 +36,7 @@ func TestAddORUpdateWorkload(t *testing.T) {
 	assert.NoError(t, err)
 	// success updat
 	err = m.UpdateWorkload(ctx, workload)
-	assert.Error(t, err, "ETCD Txn condition failed")
+	assert.NoError(t, err)
 	// success updat
 	workload.Name = "test_app_2"
 	err = m.UpdateWorkload(ctx, workload)
