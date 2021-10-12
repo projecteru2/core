b3f8bd77deefbf2b43f147c0e23732947cc7906d
zc <greyschwinger@gmail.com>
2021-10-13 02:38:10 +0800

    set explict rule for stating args to combine

--- int/testcase/gen.py
+++ int/testcase/gen.py
@@ -10,61 +10,48 @@ def itemsetter(obj, key):
     return lambda v: operator.setitem(obj, key, v)
 
 
-def is_list_without_dict(value: typing.Any) -> bool:
-    if not isinstance(value, list):
-        return False
-
-    for v in value:
-        if isinstance(v, dict):
-            return False
-        elif isinstance(v, list) and not is_list_without_dict(v):
-            return False
-
-    return True
+def need_combine(key: str) -> bool:
+    return key.endswith('@')
 
 
-def traverse_optional_args(
-    obj: dict
-) -> list[tuple[list[callable[[typing.Optional[list, dict]]]],
-                callable[[typing.Any]]]]:
+def traverse_combinators(
+    obj: typing.Union[dict, list],
+) -> list[tuple[list[typing.Any], callable[[typing.Any]]]]:
     ret = []
-    for key, value in obj.items():
-        if is_list_without_dict(value):
-            ret.append(([operator.itemgetter(key)], itemsetter(obj, key)))
 
-        elif isinstance(value, dict):
-            for getters, setter in traverse_optional_args(value):
-                ret.append(([operator.itemgetter(key)] + getters, setter))
+    if isinstance(obj, dict):
+        keys_to_rename = []
+        for key, value in obj.items():
+            if need_combine(key):
+                keys_to_rename.append(key)
+            else:
+                ret.extend(traverse_combinators(value))
 
-        elif isinstance(value, list):
-            for i, o in enumerate(value):
-                for getters, setter in traverse_optional_args(o):
-                    ret.append(([operator.itemgetter(i)] + getters, setter))
+        for old_key in keys_to_rename:
+            new_key = old_key.rstrip('@')
+            obj[new_key] = obj[old_key]
+            ret.append((value, itemsetter(obj, new_key)))
+            del obj[old_key]
 
-        else:
-            raise ValueError('invalid case request')
+    elif isinstance(obj, list):
+        for value in obj:
+            ret.extend(traverse_combinators(value))
 
     return ret
 
 
 def combine_requests(req: dict) -> typing.Generator[dict, None, None]:
-    optional_getsetter = traverse_optional_args(req)
+    combinators = traverse_combinators(req)
 
     def combine_req_for_one(idx: int) -> typing.Generator[dict, None, None]:
-        if idx == len(optional_getsetter):
+        if idx == len(combinators):
             yield req
             return
 
-        optional_args = req
-        getters, setter = optional_getsetter[idx]
-        for getter in getters:
-            optional_args = getter(optional_args)
-
-        for arg in optional_args:
+        combine_args, setter = combinators[idx]
+        for arg in combine_args:
             setter(arg)
             yield from combine_req_for_one(idx + 1)
-
-        setter(optional_args)
 
     yield from combine_req_for_one(0)
 
