fe010d9b91764a6f9ba6357b1dc4458ef2377fc5
CMGS <ilskdw@gmail.com>
2023-02-01 15:07:23 +0800

    fix tag for mapstruture

--- resource/plugins/cpumem/types/engine.go
+++ resource/plugins/cpumem/types/engine.go
@@ -2,9 +2,9 @@ package types
 
 // EngineParams .
 type EngineParams struct {
-	CPU      float64 `json:"cpu"`
-	CPUMap   CPUMap  `json:"cpu_map"`
-	NUMANode string  `json:"numa_node"`
-	Memory   int64   `json:"memory"`
-	Remap    bool    `json:"remap"`
+	CPU      float64 `json:"cpu" mapstructure:"cpu"`
+	CPUMap   CPUMap  `json:"cpu_map" mapstructure:"cpu_map"`
+	NUMANode string  `json:"numa_node" mapstructure:"numa_node"`
+	Memory   int64   `json:"memory" mapstructure:"memory"`
+	Remap    bool    `json:"remap" mapstructure:"remap"`
 }
