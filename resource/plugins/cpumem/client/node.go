3f28f1b57159b174c3f81b7195c1c78be4b62f2c
CMGS <ilskdw@gmail.com>
2023-05-26 13:25:45 +0800

    refactor cpumem addnode stage2

--- resource/plugins/cpumem/client/node.go
+++ resource/plugins/cpumem/client/node.go
@@ -2,8 +2,6 @@ package client
 
 import (
 	"encoding/json"
-	"strconv"
-	"strings"
 
 	"github.com/projecteru2/core/resource/plugins/cpumem/types"
 )
@@ -14,53 +12,23 @@ type cpuType interface {
 
 // NodeResourceRequest includes all possible fields passed by eru-core for editing node, it not parsed!
 type NodeResourceRequest[T cpuType] struct {
-	CPU        T        `json:"cpu"`
-	Share      int64    `json:"share"`
-	Memory     int64    `json:"memory"`
-	NUMA       []string `json:"numa-cpu"`
-	NUMAMemory []int64  `json:"numa-memory"`
+	CPU    T     `json:"cpu"`
+	Share  int64 `json:"share"`
+	Memory int64 `json:"memory"`
 
-	NUMACPUMap    types.NUMA       `json:"-"`
-	NUMAMemoryMap types.NUMAMemory `json:"-"`
+	NUMA       types.NUMA       `json:"numa-cpu"`
+	NUMAMemory types.NUMAMemory `json:"numa-memory"`
 }
 
 // NewNodeResourceRequest .
-func NewNodeResourceRequest[T cpuType](cpu T, share, memory int64, NUMACPUMap types.NUMA, NUMAMemoryMap types.NUMAMemory) (*NodeResourceRequest[T], error) {
+func NewNodeResourceRequest[T cpuType](cpu T, share, memory int64, NUMA types.NUMA, NUMAMemory types.NUMAMemory) (*NodeResourceRequest[T], error) {
 	r := &NodeResourceRequest[T]{
-		CPU:    cpu,
-		Share:  share,
-		Memory: memory,
+		CPU:        cpu,
+		Share:      share,
+		Memory:     memory,
+		NUMA:       NUMA,
+		NUMAMemory: NUMAMemory,
 	}
-	tmpNUMA := map[int][]string{}
-	for cpuID, nodeID := range NUMACPUMap {
-		nID, err := strconv.Atoi(nodeID)
-		if err != nil {
-			return nil, err
-		}
-		if tmpNUMA[nID] == nil {
-			tmpNUMA[nID] = []string{cpuID}
-			continue
-		}
-		tmpNUMA[nID] = append(tmpNUMA[nID], cpuID)
-	}
-	r.NUMA = make([]string, len(tmpNUMA))
-	for nodeID, cpus := range tmpNUMA {
-		r.NUMA[nodeID] = strings.Join(cpus, ",")
-	}
-
-	tmpNUMAMemory := map[int]int64{}
-	for nodeID, memory := range NUMAMemoryMap {
-		nID, err := strconv.Atoi(nodeID)
-		if err != nil {
-			return nil, err
-		}
-		tmpNUMAMemory[nID] = memory
-	}
-	r.NUMAMemory = make([]int64, len(tmpNUMAMemory))
-	for nodeID, memory := range tmpNUMAMemory {
-		r.NUMAMemory[nodeID] = memory
-	}
-
 	return r, nil
 }
 
