92c95290de2f935e8f849216010459fda453c8ec
YuYang <yyangplus@gmail.com>
2023-09-21 15:22:34 +0800

    call plugin in parallel

--- resource/cobalt/call.go
+++ resource/cobalt/call.go
@@ -2,6 +2,7 @@ package cobalt
 
 import (
 	"context"
+	"sync"
 
 	"github.com/cockroachdb/errors"
 	"github.com/projecteru2/core/log"
@@ -9,19 +10,28 @@ import (
 )
 
 func call[T any](ctx context.Context, ps []plugins.Plugin, f func(plugins.Plugin) (T, error)) (map[plugins.Plugin]T, error) {
-	// TODO 并行化，意义不大
+	var mu sync.Mutex
+	var wg sync.WaitGroup
 	var combinedErr error
 	results := map[plugins.Plugin]T{}
 
 	for _, p := range ps {
-		result, err := f(p)
-		if err != nil {
-			log.WithFunc("resource.cobalt.call").Errorf(ctx, err, "failed to call plugin %+v", p.Name())
-			combinedErr = errors.CombineErrors(combinedErr, errors.Wrap(err, p.Name()))
-			continue
-		}
-		results[p] = result
-	}
+		wg.Add(1)
+		go func(p plugins.Plugin) {
+			defer wg.Done()
 
+			result, err := f(p)
+			mu.Lock()
+			defer mu.Unlock()
+
+			if err != nil {
+				log.WithFunc("resource.cobalt.call").Errorf(ctx, err, "failed to call plugin %+v", p.Name())
+				combinedErr = errors.CombineErrors(combinedErr, errors.Wrap(err, p.Name()))
+				return
+			}
+			results[p] = result
+		}(p)
+	}
+	wg.Wait()
 	return results, combinedErr
 }
