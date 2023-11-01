81a7e5a2c3508025507fe5c2f5839f93223a9f10
YuYang <yyangplus@gmail.com>
2023-11-01 16:34:18 +0800

    add script to build go plugin

---
+++ build-goplugin.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+pkg=$1
+codeDIR=$2
+if [ -z "$pkg" ]; then
+    echo "error: package name is null"
+    echo "usage: "
+    echo "     bash build-goplugin.sh pkg codeDir"
+    exit 1
+fi 
+
+if [ -z "$codeDir" ]; then
+    echo "error: plugin code dir is null"
+    echo "usage: "
+    echo "     bash build-goplugin.sh pkg codeDir"
+    exit 1
+fi
+echo "Building $pkg($codeDIR)"
+
+go mod tidy
+go mod vendor
+
+tee -a go.mod <<EOF
+require $pkg v0.0.0-00010101000000-000000000000
+replace $pkg => $codeDIR
+EOF
+
+go build -buildmode=plugin -mod=readonly $pkg
+
+git checkout -- go.mod
