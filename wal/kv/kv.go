6b8f56797c4f578611ea8027c66f8df8b7f2d2a3
CMGS <ilskdw@gmail.com>
2026-08-25 07:54:22 +0800

    cut: inline the single-use wal and kv sub-interfaces

--- wal/kv/kv.go
+++ wal/kv/kv.go
@@ -7,29 +7,13 @@ import (
 
 // KV is the key/value store backing the WAL.
 type KV interface {
-	OpenCloser
-	Simpler
-	Scanner
-	Sequencer
-}
-
-type Simpler interface {
+	Open(path string, mode os.FileMode, timeout time.Duration) error
+	Close() error
 	Put([]byte, []byte) error
 	Get([]byte) ([]byte, error)
 	Delete([]byte) error
-}
-
-type Scanner interface {
 	Scan([]byte) (<-chan ScanEntry, func())
-}
-
-type Sequencer interface {
 	NextSequence() (ID uint64, err error)
-}
-
-type OpenCloser interface {
-	Open(path string, mode os.FileMode, timeout time.Duration) error
-	Close() error
 }
 
 type ScanEntry interface {
--- wal/wal.go
+++ wal/wal.go
@@ -8,25 +8,9 @@ const eventPrefix = "/events/"
 
 // WAL logs an event before its operation runs and replays the unfinished ones on recovery.
 type WAL interface {
-	Registry
-	Recoverer
-	Logger
-	Closer
-}
-
-type Recoverer interface {
-	Recover(context.Context)
-}
-
-type Registry interface {
 	Register(EventHandler)
-}
-
-type Logger interface {
+	Recover(context.Context)
 	Log(string, any) (Commit, error)
-}
-
-type Closer interface {
 	Close() error
 }
 
